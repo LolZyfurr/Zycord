01f4289e1f2675a4af28f7b19851096112f8b27a
Zy1ux <132412672+Zy1ux@users.noreply.github.com>
2024-03-07 11:14:29 -0700

    Update MainClient.js

--- MainClient.js
+++ MainClient.js
@@ -6,7 +6,7 @@
     let MONTH_UPDATED = 3
     let DAY_UPDATED = 7
     let YEAR_UPDATED = 24
-    let MINUTES_UPDATED = 7
+    let MINUTES_UPDATED = 15
     let TIME_UPDATED = 11
 
     let ALPHA_MONTH = String.fromCharCode(MONTH_UPDATED + 64) // Convert to letter A-Z
