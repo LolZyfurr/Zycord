21b8fb5275daeca02fce9a08cf1ae90f51811f4c
Zy1ux <132412672+Zy1ux@users.noreply.github.com>
2024-03-08 16:05:12 -0700

    Fixed UI size config.

--- MainClient.js
+++ MainClient.js
@@ -500,7 +500,7 @@ body {
         const buttonSettings = leaderboardDebounce ? '128,128,128,1' : '0,0,0,0';
         button.style.backgroundColor = `rgba(${buttonSettings})`;
     }
-    let TOPBAR_SIZE = 11
+    let TOPBAR_SIZE = 33
     let SETTINGS_ICON = 'https://media.discordapp.net/attachments/1205660691500105730/1215348125317992580/file.png?ex=65fc6c25&is=65e9f725&hm=2c61be850593bed1f81e04c4ba0a49a9d9af647f7b9ebc69fcd979eb89063e6e&=&format=webp&quality=lossless';
     let LIGHT_THEME_ICON = 'https://media.discordapp.net/attachments/1205660691500105730/1215353120599707688/file.png?ex=65fc70cc&is=65e9fbcc&hm=a0aaf2943817e47eb1a55b23b2d2ed8015cf96bba85dffa3b526170518af3dee&=&format=webp&quality=lossless';
     let LEADERBOARD_ICON = 'https://media.discordapp.net/attachments/1205660691500105730/1215352625164197939/file.png?ex=65fc7055&is=65e9fb55&hm=db2390e46423d91536a94fff1aa11a22f2387d60f3032155dd5ca756323f1d7f&=&format=webp&quality=lossless';
