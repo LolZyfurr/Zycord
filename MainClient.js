e53e21f8bb9178d807bbc4ddc0d7f14694fe9bbc
Zy1ux <132412672+Zy1ux@users.noreply.github.com>
2024-04-01 09:59:39 -0700

    Fixed the ShadeWeb function.

--- MainClient.js
+++ MainClient.js
@@ -7,7 +7,7 @@
     let MONTH_UPDATED = 4
     let DAY_UPDATED = 1
     let YEAR_UPDATED = 24
-    let MINUTES_UPDATED = 52
+    let MINUTES_UPDATED = 59
     let TIME_AFTERNOON = 0
     let TIME_UPDATED = 9
 
@@ -154,7 +154,7 @@
     function ShadeWeb(e) {
         WatermarkWeb(`ZYCORD ${APP_VERSION}`, "#FFFFFF");
         var n = document.getElementById("shadeWebStyle"),
-            t = `\nbody::before {\ncontent: "";\nposition: fixed;\ntop: 0;\nleft: 0;\nwidth: 100%;\nheight: 100%;\nz-index: 999998;\npointer-events: none;\n${BLUR_WEB === true ? (`backdrop-filter: blur(${20/e}px);`) : (`background-color: rgba(0,0,0,${e});`)}\nbackground-image: url('');\nbackground-size: cover;\n}\n`;
+            t = `\nbody::before {\ncontent: "";\nposition: fixed;\ntop: 0;\nleft: 0;\nwidth: 100%;\nheight: 100%;\nz-index: 999998;\npointer-events: none;\n${BLUR_WEB === true ? (`backdrop-filter: blur(${20/(1-e)}px);`) : (`background-color: rgba(0,0,0,${e});`)}\nbackground-image: url('');\nbackground-size: cover;\n}\n`;
         if (n) n.innerHTML = t;
         else {
             var d = document.createElement("style");
