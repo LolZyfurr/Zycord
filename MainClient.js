b5a429177f272def96f6bac3a885259d75334606
Zy1ux <132412672+Zy1ux@users.noreply.github.com>
2024-05-08 11:24:33 -0700

    Update MainClient.js

--- MainClient.js
+++ MainClient.js
@@ -287,7 +287,7 @@
         let closeIconSvgPath = `M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z`;
         let notificationContainer = document.querySelector('div.base_c0676e');
         let newNotification = document.createElement('div');
-        let notificationHTML = `<div class="notification__5fd4c" style="background-color: ${notificationColor}; color: var(--white-500);"> <div id="${notificationCloseId}" class="closeButton__90904" aria-label="Dismiss" role="button" tabindex="0"><svg aria-hidden="true" role="img" class="closeIcon__967e7" width="18" height="18" viewBox="0 0 24 24"> <path fill="currentColor" d="${closeIconSvgPath}"> </path> </svg> </div>${notificationMessage}<button id="${notificationDismissId}" class="button__33db6">Dismiss</button> </div>`
+        let notificationHTML = `<div class="notice__5fd4c" style="background-color: ${notificationColor}; color: var(--white-500);"> <div id="${notificationCloseId}" class="closeButton__90904" aria-label="Dismiss" role="button" tabindex="0"><svg aria-hidden="true" role="img" class="closeIcon__967e7" width="18" height="18" viewBox="0 0 24 24"> <path fill="currentColor" d="${closeIconSvgPath}"> </path> </svg> </div>${notificationMessage}<button id="${notificationDismissId}" class="button__33db6">Dismiss</button> </div>`
         notificationContainer.appendChild(newNotification);
         newNotification.id = notificationMainId;
         newNotification.innerHTML = notificationHTML;
